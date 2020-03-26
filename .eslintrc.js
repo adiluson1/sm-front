c26de9055183359882201d88fab5f82f6b1e79b9
adiluson1 <adil.uson.kg@gmail.com>
2020-03-26 18:32:13 +0300

    init in remote

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+
 module.exports = {
   root: true,
   env: {
@@ -6,14 +7,16 @@ module.exports = {
   'extends': [
     'plugin:vue/essential',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript'
   ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'off' : 'off',
+    'no-case-declarations': 'off',
+    'no-inner-declarations': 'off'
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
   },
   overrides: [
     {
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from '@/App.tsx'
 import './registerServiceWorker'
 import router from './router'
+import AntDesignVue from 'ant-design-vue'
 
-Vue.config.productionTip = false
+import {i18n} from "@/lang";
+
+import 'ant-design-vue/dist/antd.css';
+
+Vue.config.productionTip = false;
+Vue.use(AntDesignVue);
+
 
 new Vue({
   router,
+  i18n,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
